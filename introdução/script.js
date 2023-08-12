1b5ccabcf19531cab6ad7d554963ec752d7f0173
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-08-11 22:51:19 -0300

    organizando pastas

---
+++ introdução/script.js
@@ -0,0 +1,7 @@
+document.getElementById("titulo").innerHTML = "opa tudo bem?";
+
+document.write("olá pedro");
+
+window.alert("teste aqui, ok");
+
+console.log("A menasgem que eu inserir aqui, aparecerá no console do site");
