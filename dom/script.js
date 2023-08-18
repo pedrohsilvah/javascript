77fdca0caf41979876fefb55eebacdac0e11012f
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-08-17 23:11:23 -0300

    treinando dom classlist

--- dom/script.js
+++ dom/script.js
@@ -1,11 +1,37 @@
-function verde() {
+function trocar() {
+    if (document.querySelector('button').classList.contains('preto')) {
+        document.querySelector('button').classList.remove('preto');
+        document.querySelector('button').classList.add('verde');
+    } else {
+        document.querySelector('button').classList.remove('verde');
+        document.querySelector('button').classList.add('preto');
+    }
+}
 
+
+
+
+
+
+
+
+
+
+
+/*function verde() {
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector("#exemplo").classList.add('verde');
 }
 
 function vermelho() {
-
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.remove('verde')
+    document.querySelector("#exemplo").classList.add('vermelho');
 }
 
 function azul() {
-    
-}
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.remove('verde')
+    document.querySelector("#exemplo").classList.add('azul');
+} */
