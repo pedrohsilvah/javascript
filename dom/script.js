7615c29f3d64f93b221e6d484e9687e5361a3ad2
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-08-18 22:31:36 -0300

    treinando funções do js

--- dom/script.js
+++ dom/script.js
@@ -1,6 +1,25 @@
-function alterar() {
-     
+function alterar(titulo) {
+     document.getElementById("titulo").innerHTML = titulo
+     document.getElementById("campo").value = titulo
 }
+
+function somar(x, y) {
+    let total = x + y
+
+    return total;
+}
+
+var resultado = somar(10, 15);
+
+console.log(resultado) 
+
+
+
+
+
+
+
+
 
 
 /*function trocar() {
