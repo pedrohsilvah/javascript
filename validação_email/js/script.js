fd53048bf9a0efaf343c3c111758818d1185451d
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-09-02 19:54:18 -0300

    criando base de form

---
+++ validação_email/js/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const massageTexarea = document.querySelector("#massage");
+
+console.log(form, nameInput, emailInput);
