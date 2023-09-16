6c4b2be610b4cb098ca792a01db308f9e3712ea0
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-09-16 19:13:13 -0300

    criando validação de e-mail

--- validação_email/js/script.js
+++ validação_email/js/script.js
@@ -2,6 +2,37 @@ const form = document.querySelector("#form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
-const massageTexarea = document.querySelector("#massage");
+const massageTextarea = document.querySelector("#massage");
 
-console.log(form, nameInput, emailInput);
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+// Verifica se o nome está vazio
+    if(nameInput.value === "") {
+        alert("Por favor, preencha o seu nome")
+        return;
+    }
+
+// Verificar se o e-mail está prenchido e se é válido
+    if(emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("Por favor, preencha o seu e-mail")
+        return;
+    }
+
+// Se todos os campos estiverem corretamente preenchidos envie o form
+    form.submit();
+});
+
+function isEmailValid(email) {
+//Cria regex para validar email
+const emailRegex = new RegExp(
+    //usuario12@host.com.br
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+);
+
+    if(emailRegex.test(email)){
+        return true;
+    }
+
+    return false;
+}
