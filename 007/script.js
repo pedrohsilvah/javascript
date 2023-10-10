33557b89dd350274a4d13a636c9d1e9d5b9a9e20
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-10-10 11:57:23 -0300

    revisando operadores em js

---
+++ 007/script.js
@@ -0,0 +1,13 @@
+var nome = "João Ribeiro"
+//console.log(nome)
+//alert(nome)
+document.write(nome)
+
+// var valor1 = 2
+// var valor2 = 3
+// let soma = valor1 += valor2
+// console.log(soma)
+
+nome = "Pedro"
+sobrenome = " Henrique"
+console.log(nome += sobrenome)
