2b29b33aa86946b47f2bb07363e1219fe4c4fdd4
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-10-10 23:10:32 -0300

    estudando js

--- 007/script.js
+++ 007/script.js
@@ -1,13 +1,25 @@
-var nome = "João Ribeiro"
+// var nome = "João Ribeiro"
 //console.log(nome)
 //alert(nome)
-document.write(nome)
+// document.write(nome)
 
 // var valor1 = 2
 // var valor2 = 3
 // let soma = valor1 += valor2
 // console.log(soma)
 
-nome = "Pedro"
-sobrenome = " Henrique"
-console.log(nome += sobrenome)
+// nome = "Pedro"
+// sobrenome = " Henrique"
+// console.log(nome += sobrenome)
+
+
+// let frase = 'Pedro\ns é mt lindo!'
+// console.log(frase.indexOf('lindo!'))
+
+let numero = 20
+
+if(numero < 10){
+    console.log('Este número é menor que 10.')
+} else {
+    console.log('Este número é maior ou igual a 10.')
+}
