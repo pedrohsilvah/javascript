df497da52080bfa92a5060a24e2027f85e05b7cd
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-10-11 22:29:47 -0300

    estudando condições js

--- 007/script.js
+++ 007/script.js
@@ -16,10 +16,33 @@
 // let frase = 'Pedro\ns é mt lindo!'
 // console.log(frase.indexOf('lindo!'))
 
-let numero = 20
+// let numero = 20
 
-if(numero < 10){
-    console.log('Este número é menor que 10.')
-} else {
-    console.log('Este número é maior ou igual a 10.')
-}
+// if(numero < 10){
+//     console.log('Este número é menor que 10.')
+// } else {
+//     console.log('Este número é maior ou igual a 10.')
+// }
+
+// let valor1 = 100
+// let valor2 = "10"
+
+// if(valor1 !== valor2){
+//     console.log("Os valores são iguais.")
+// }
+
+// let valor1 = 20;
+// if(valor1 == 20 || valor1 > 100){
+//     console.log('O valor é igual a 20 ou superior a 100')
+// }
+
+// if((10 + 20 > 30) || (40 + 50 < 100)){
+//     console.log("Pelo menos uma das condições é verdadeira ")
+// }
+
+
+let valor = 10
+let frase = valor < 5 ? 'SIM' : 'NÃO'
+console.log(frase)
+
+ 
