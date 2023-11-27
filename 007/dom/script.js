65b1688b7fcdc234d10bbe19caacc68aa3ff427c
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-11-27 11:42:27 -0300

    estudos de dom

--- 007/dom/script.js
+++ 007/dom/script.js
@@ -23,9 +23,14 @@
 // link.setAttribute('href', 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.petz.com.br%2Fblog%2F%2Fwp-content%2Fuploads%2F2021%2F11%2Fenxoval-para-gato-Copia.jpg&tbnid=vJ0O0mEfKlj4CM&vet=12ahUKEwjh0veznNWCAxWlTrgEHXzfC6AQMygIegQIARB_..i&imgrefurl=https%3A%2F%2Fwww.petz.com.br%2Fblog%2Fbem-estar%2Fenxoval-para-gato%2F&docid=GfY1AYGJ6B5EYM&w=750&h=450&q=imagem%20gatos&ved=2ahUKEwjh0veznNWCAxWlTrgEHXzfC6AQMygIegQIARB_');
 
 // console.log(link.getAttribute('href'));
-let novo_elemento = document.createElement('p')
+// let novo_elemento = document.createElement('p')
 
-let div = document.querySelector('#elemento_1')
-novo_elemento.innerText = 'Texto do parágrafo'
+// let div = document.querySelector('#elemento_1')
+// novo_elemento.innerText = 'Texto do parágrafo'
 
-div.appendChild(novo_elemento)
+// div.appendChild(novo_elemento)
+
+let div1 = document.querySelector('#elemento_1')
+let novo_paragrafo = document.createElement('p').innerText = 'olá mundo!!'
+
+div1.prepend(novo_paragrafo)
