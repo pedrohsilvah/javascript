a570596108dda9558f0349727463507cfeb94b90
pedro <pedrohsilvasant@gmail.com>
2024-02-04 19:50:58 -0300

    estudando componentes

--- angular/desafio-01/src/components/cardNews.js
+++ angular/desafio-01/src/components/cardNews.js
@@ -4,7 +4,7 @@ class CardNews extends HTMLElement {
 
         const shadow = this.attachShadow({mode: "open"})
         shadow.appendChild(this.build())
-        shadow.appendChild(this.style())
+        shadow.appendChild(this.styles())
      }
 
      build() {
@@ -19,6 +19,7 @@ class CardNews extends HTMLElement {
 
         const linkTitle = document.createElement("a")
         linkTitle.textContent = this.getAttribute("title")
+        linkTitle.href = this.getAttribute("link-url")
 
         const newsContent = document.createElement("p")
         newsContent.textContent = this.getAttribute("content") 
@@ -31,6 +32,8 @@ class CardNews extends HTMLElement {
         const cardRight = document.createElement("div")
         componentRoot.setAttribute("class", "card_right")
         const newsImage = document.createElement("img")
+        newsImage.src = this.getAttribute("photo") || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1sd3YdG1rJy9lvR3AS_AwYH13ZdkNkjcOEJC9v2-MIg&s"
+        newsImage.alt = "Foto da noticia"
         cardRight.appendChild(newsImage)
 
         componentRoot.appendChild(cardLeft)
@@ -39,7 +42,45 @@ class CardNews extends HTMLElement {
         return componentRoot
      }
 
-     styles() {}
+     styles() {
+      const style = document.createElement("style")
+      style.textContent = `
+  .card {
+      width: 80%;
+      border: 1px solid gray;
+      box-shadow: 6px 6px 14px black;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+  }
+  
+  .card_left {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding-left: 10px;
+  }
+  
+  .card-left > span {
+      font-weight: 400;
+  }
+  
+  .card_left > h1 {
+      margin-top: 15px;
+      font-size: 25px;
+  }
+  
+  .card_left > p {
+      color: gray;
+  }
+  
+  img {
+      max-width: 200px;
+  }  
+      `
+
+      return style
+     }
 }
 
 customElements.define("card-news", CardNews)
